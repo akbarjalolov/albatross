a403e2836113455d1953b101e03b6c81627d715f
Ruslan Ubaydullayev <r.ubaydullayev@uic.group>
2022-12-21 00:22:12 +0500

    feat: blogs

--- components/sections/Blogs.vue
+++ components/sections/Blogs.vue
@@ -1,47 +1,54 @@
 <template>
-    <div class="container mx-auto relative">
+    <div class="relative">
         <img class="absolute left-[1%] top-[7%]" src="@/static/img/arrow-1.png" alt="" />
-        <section-title title="Blogs" />
-        <div class="container mx-auto">
-            <div class="grid grid-cols-12">
-                <div class="col-span-7">
-                    <div class="thumbnail-blog relative">
-                    <img class="absolute w-full h-full top-0 left-0 object-cover" :src="blogs[0].img" :alt="blogs[0].title">
-                    <img src="@/static/img/blog-shadow.png" class="absolute w-full h-full top-0 left-0" alt="">
-                    <div class="relative">
-                        <h4 class="text-white leading-130 text-[28px] font-medium">
-                            {{blogs[0].person}}
-                        </h4>
-                        <h5 class="text-lightGray leading-130 text-[16px] font-normal mt-[4px]">
-                        {{blogs[0].position}}
-                        </h5>
-                        <h4 class="mt-[160px] text-white leading-130 text-[28px] font-medium">
-                            {{blogs[0].title}}
-                        </h4>
-                        <button class="flex items-center mt-20 gap-[12px] group">
-                            <h4 class="text-dBlue text-[20px] leading-130 font-normal">Learn more</h4>
-                           <img class="border border-solid border-dBlue duration-200 rounded-full w-[28px] p-4 h-auto object-cover group-hover:translate-x-1" src="@/static/icons/arrow-right-blue.svg"/>
-                        </button>
+        <div class="container mx-auto py-[60px]">
+            <section-title title="Blogs" />
+            <div class="container mx-auto mt-32">
+                <div class="grid grid-cols-12 gap-[20px]">
+                    <div class="col-span-7">
+                        <nuxt-link :to="`/blogs/${blogs[0].slug}`" class="thumbnail-blog block relative">
+                        <img class="absolute w-full h-full top-0 left-0 object-cover" :src="blogs[0].img" :alt="blogs[0].title">
+                        <img src="@/static/img/blog-shadow.png" class="absolute w-full h-full top-0 left-0" alt="">
+                        <div class="relative">
+                            <h4 class="text-white leading-130 text-[28px] font-medium">
+                                {{blogs[0].person}}
+                            </h4>
+                            <h5 class="text-lightGray leading-130 text-[16px] font-normal mt-[4px]">
+                            {{blogs[0].position}}
+                            </h5>
+                            <h4 class="mt-[160px] text-white leading-130 text-[28px] font-medium">
+                                {{blogs[0].title}}
+                            </h4>
+                            <button class="flex items-center mt-20 gap-[12px] group">
+                                <h4 class="text-dBlue text-[20px] leading-130 font-normal">Learn more</h4>
+                            <img class="border border-solid border-dBlue duration-200 rounded-full w-[28px] p-4 h-auto object-cover group-hover:translate-x-1" src="@/static/icons/arrow-right-blue.svg"/>
+                            </button>
+                        </div>
+                    </nuxt-link>
+                    </div>
+                    <div class="col-span-5">
+                        <div class="flex flex-col bg-white blogs rounded-2xl p-[28px]">
+                            <nuxt-link :to="`/blogs/${item.slug}`" v-for="(item,index) in blogs.slice(1,4)" :key="index" class="flex justify-center align-items-center gap-4 group border-b border-solid border-[#E6ECF0] last:border-b-0 py-[20px]">
+                                <img :src="item.img" class="min-w-[80px] h-[48px] object-cover rounded-[4px] border border-solid border-[#E6ECF0]" alt="">
+                                <div class="flex flex-col">
+                                    <h4 class="text-dark duration-300 group-hover:text-dBlue font-medium text-[18px] leading-130">{{item.title}}</h4>
+                                    <div class="flex items-center mt-[8px] gap-[6px]">
+                                        <img src="@/static/icons/user.svg" alt="">
+                                        <h6 class="text-[#74787D] font-normal text-[14px] leading-130"   >{{item.person}}</h6>
+                                    </div>
+                                </div>
+                            </nuxt-link>
+                            <nuxt-link to="/blogs" class="flex items-center mt-20 gap-[12px] group duration-300 hover:gap-[20px]">
+                                <h4 class="text-dBlue text-[20px] leading-130 font-normal">See all</h4>
+                            <img class="border border-solid border-dBlue duration-200 rounded-full w-[28px] p-4 h-auto object-cover group-hover:translate-x-1" src="@/static/icons/arrow-right-blue.svg"/>
+                            </nuxt-link>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-span-5">
-                    <div class="flex flex-col bg-white shadow-sm rounded-2xl p-[28px]">
-                        <div v-for="(item,index) in blogs" :key="index" class="flex justify-center align-items-center gap-4">
-                            <img :src="item.img" class="w-[80px] h-[48px] object-cover" alt="">
-                            <div class="flex flex-col">
-                                <h4>{{item.title}}</h4>
-                                <div class="flex itemss-center">
-                                    <img src="@/static/icons/user.svg" alt="">
-                                    <h6>{{item.person}}</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-        </div>
         </div>
     </div>
+
 </template>
 
 <script>
@@ -55,25 +62,29 @@ import SectionTitle from '../SectionTitle.vue'
                         title: 'Lowest Quote Isn’t Always the Best Option Car Shipping',
                         person: 'David Branson',
                         position: 'Account executive, Auto Transport Expert',
-                        img: 'https://picsum.photos/500/500'
+                        img: 'https://picsum.photos/650/650',
+                        slug: 'news1'
                     },
                     {
                         title: 'Lowest Quote Isn’t Always the Best Option Car Shipping',
                         person: 'David Branson',
                         position: 'Account executive, Auto Transport Expert',
-                        img: 'https://picsum.photos/500/500'
+                        img: 'https://picsum.photos/600/600',
+                        slug: 'news2'
                     },
                     {
                         title: 'Lowest Quote Isn’t Always the Best Option Car Shipping',
                         person: 'David Branson',
                         position: 'Account executive, Auto Transport Expert',
-                        img: 'https://picsum.photos/500/500'
+                        img: 'https://picsum.photos/500/500',
+                        slug: 'news3'
                     },
                     {
                         title: 'Lowest Quote Isn’t Always the Best Option Car Shipping',
                         person: 'David Branson',
                         position: 'Account executive, Auto Transport Expert',
-                        img: 'https://picsum.photos/500/500'
+                        img: 'https://picsum.photos/400/400',
+                        slug: 'news4'
                     }
                 ]
             }
@@ -86,6 +97,10 @@ import SectionTitle from '../SectionTitle.vue'
         overflow: hidden;
         background: linear-gradient(180deg, rgba(34, 35, 44, 0.6) 0%, #22232C 100%);
         padding: 28px;
+        border-radius: 16px;
+    }
+    .blogs {
+        box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.06);
         border-radius: 16px;
     }
 </style>
