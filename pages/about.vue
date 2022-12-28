6d8b59ebb813adc0259c090f7c27f59faa88955f
Ruslan Ubaydullayev <r.ubaydullayev@uic.group>
2022-12-29 03:55:08 +0500

    our team: about

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <!-- target -->
         <div class="w-full max-w-[1500px] mx-auto flex items-center justify-between gap-[40px]">
             <div class="pl-[140px]">
                 <h4 class="font-medium text-dark text-[64px] leading-[82px]">We build bridges between <span class="font-normal text-[64px] leading-[82px] text-dark opacity-80">drivers and customers</span></h4>
@@ -9,9 +10,11 @@
                 <img class="max-w-[550px] w-full object-cover" src="@/static/img/target.png" alt="">
             </div>
         </div>
+        <!-- truck img -->
         <div class="w-full max-h-[640px] mt-[60px]">
             <img class="w-full h-full object-cover" src="@/static/img/truck-about.png" alt="">
         </div>
+        <!-- cards -->
         <div class="bg-gray-100 py-[60px]">
             <div class="container mx-auto px-[16px]">
                 <div class="grid grid-cols-12">
@@ -25,11 +28,23 @@
                 </div>
             </div>
         </div>
+        <!-- our team -->
+        <div class="bg-white py-[60px]">
+            <section-title title="Our Team" />    
+            <div class="container px-[16px] mt-[32px] mx-auto">
+                <div class="grid grid-cols-4 gap-x-[20px] gap-y-[24px]">
+                        <TeamCard v-for="(item,index) in ourTeam" :key="index" :person="item"/>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import SectionTitle from '../components/SectionTitle.vue'
+import TeamCard from '../components/cards/TeamCard.vue'
     export default {
+  components: { SectionTitle, TeamCard },
         data(){
             return {
                 aboutInfo: [
@@ -37,6 +52,48 @@
                     { title: '12k+', desc: 'Vehicle shipped' },
                     { title: '8k+', desc: 'Online reviews' },
                     { title: '10+', desc: 'Years in operation' },
+                ],
+                ourTeam: [
+                    {
+                        img: 'https://picsum.photos/300/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/200/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/400/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/500/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/700/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/300/500',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/500/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    },
+                    {
+                        img: 'https://picsum.photos/1000/300',
+                        full_name: 'John Doe',
+                        position: 'CEO',
+                    }
                 ]
             }
         }
